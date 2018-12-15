5c49117ca1495304916679f9c9c31e2398167c19
ToddWebDev <webdevtodd@gmail.com>
2018-12-15 13:30:09 -0500

    Updates

---
+++ precache-manifest.5cb9b6179344516e6f16cdbe6b08abe1.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "a18fdffd416bf30304db",
+    "url": "/open-snow/static/css/main.17034948.chunk.css"
+  },
+  {
+    "revision": "a18fdffd416bf30304db",
+    "url": "/open-snow/static/js/main.a18fdffd.chunk.js"
+  },
+  {
+    "revision": "82dafdb578ba27673946",
+    "url": "/open-snow/static/js/1.82dafdb5.chunk.js"
+  },
+  {
+    "revision": "7ddd4c9488dfb251a8c3",
+    "url": "/open-snow/static/js/runtime~main.7ddd4c94.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/open-snow/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "a67875d25c8c2da2b4e5d281eba1c34a",
+    "url": "/open-snow/index.html"
+  }
+];
