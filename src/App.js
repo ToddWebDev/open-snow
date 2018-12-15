17eab6c3686ec25eccc6257f80ae276c67ba5856
ToddWebDev <webdevtodd@gmail.com>
2018-12-15 13:53:17 -0500

    Brand Open Snow

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
+import logo from './snowflake-shape.svg';
 import './App.css';
 
 class App extends Component {
@@ -8,17 +8,16 @@ class App extends Component {
       <div className="App">
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
+          <h1>Open Snow</h1>
+          <p>Get out there.</p>
+          {/* <a
             className="App-link"
             href="https://reactjs.org"
             target="_blank"
             rel="noopener noreferrer"
           >
             Learn React
-          </a>
+          </a> */}
         </header>
       </div>
     );
