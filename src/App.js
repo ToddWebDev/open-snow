08ef3eeae5ca0b213f3f8057093051d3bf169bc7
ToddWebDev <webdevtodd@gmail.com>
2018-12-18 13:13:51 -0500

    Add navbar and jumbotron

--- src/App.js
+++ src/App.js
@@ -3,23 +3,38 @@ import logo from './snowflake-shape.svg';
 import './App.css';
 import './bootstrap.min.css';
 
+function NavBar() {
+  return(
+    <div className="navbar navbar-dark bg-primary">
+      <a className="navbar-brand" href="#">
+        <img src={logo} width="30" height="30" className="App-logo d-inline-block align-top" alt="logo" />
+        &nbsp;Open Snow
+      </a>
+    </div>
+  );
+}
+
+function Hero() {
+  return(
+    <div className="row" style={{marginTop: 50}}>
+      <div className="jumbotron col-10 offset-1">
+        <h1>Open Snow</h1>
+        <p>Get out there.</p>
+        <hr className="my-4"/>
+        <a className="btn btn-primary btn-lg" href="#" role="button">View Reports</a>
+      </div>
+    </div>
+  );
+}
+
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1>Open Snow</h1>
-          <p>Get out there</p>
-          {/* <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a> */}
-        </header>
+      <div>
+        <NavBar/>
+        <div className="container-fluid">
+          <Hero/>
+        </div>
       </div>
     );
   }
